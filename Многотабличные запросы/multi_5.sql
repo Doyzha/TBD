9eab5cb5cfde97f68133c366bf12590343b13fac
Doyzha <90182176+Doyzha@users.noreply.github.com>
2022-05-29 16:35:11 +0300

    Update multi_5.sql

--- Многотабличные запросы/multi_5.sql
+++ Многотабличные запросы/multi_5.sql
@@ -1,13 +1,33 @@
-SELECT st.id, st.name, st.surname, st.birthday
-FROM student st,
-  (SELECT st.id
-  FROM student st
-  INNER JOIN
-    (SELECT *
-      FROM student_hobby sth
-      WHERE sth.finished_at IS NOT NULL) sth
-  ON st.id = sth.student_id
-  GROUP BY st.id
-  HAVING COUNT(st.id) > 1) multhobby
+SELECT
+	st.name, 
+	st.surname, 
+	st.id, 
+	st.birthday, 
+	h.name, 
+	sth.finished_at,
+	(DATE_PART('year', CAST(CURRENT_DATE AS date)) - DATE_PART('year', st.birthday)) AS age
+FROM 
+	student st, 
+	student_hobby sth, 
+	hobby h,
+	(SELECT st.id
+	FROM student st, 
+	student_hobby sth, 
+	hobby h
+	WHERE st.id = sth.student_id 
+	 AND sth.hobby_id = h.id 
+	 AND sth.finished_at IS NULL
+	GROUP BY st.id) AS hobby_sum
 WHERE 
-  EXTRACT(DAYS FROM NOW() - st.birthday)/365 > 19 AND st.id = multhobby.id
+	st.id = sth.student_id 
+	AND sth.hobby_id = h.id
+	AND (DATE_PART('year', CAST(CURRENT_DATE AS date)) - DATE_PART('year', st.birthday)) >= 19
+	AND sth.finished_at IS NULL
+	AND hobby_sum.id = st.id
+GROUP BY 
+	st.name, 
+	st.surname, 
+	st.id, 
+	st.birthday, 
+	h.name, 
+	sth.finished_at
