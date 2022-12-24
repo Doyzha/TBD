b89ece4ac1eeff20f84b8dc073756cb853b644f2
Doyzha <90182176+Doyzha@users.noreply.github.com>
2022-12-24 21:23:53 +0300

    Update main.go

--- RPBD/people_service/main.go
+++ RPBD/people_service/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/RyabovNick/databasecourse_p2/golang/tasks/people_service/service/store"
+	"github.com/Doyzha/TBD/tree/people_service/RPBD/people_service/service/store"
 )
 
 func main() {
-	conn := "postgresql://tselikova:284463@95.217.232.188:7777/tselikova"
+	conn := "postgresql://doizha:ablbhektn@95.217.232.188:7777/doizha"
 	s := store.NewStore(conn)
 	fmt.Println(s.ListPeople())
 	fmt.Println(s.GetPeopleByID(3))
